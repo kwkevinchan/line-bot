082123cd87c3bfa745f517147fe646c9d74f2058
kwkevin <kwkevin.chan@gmail.com>
2019-06-08 01:01:21 +0800

    docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
 
-if  [ ${USE_CP} ]
+if  ${USE_CP}
 then
     echo "use cp"
-    rm -r ${SERVICE_PATH}
-    cp -r /app ${SERVICE_PATH}/
+    cp -rf /app ${SERVICE_PATH}/
     chown -R 1000:1000 ${SERVICE_PATH}/
 else
     echo "not use cp"
