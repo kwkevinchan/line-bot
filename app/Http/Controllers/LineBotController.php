a4599eeeb8e6fa7f0f5d3329e35173a229cf729c
kwkevin <kwkevin.chan@gmail.com>
2019-06-09 00:56:03 +0800

    line webhook

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use LINE\LINEBot\LINEBot;
+use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use Illuminate\Support\Facades\Storage;
 
 class LineBotController extends Controller
 {
@@ -14,11 +15,16 @@ class LineBotController extends Controller
         $httpClient = new CurlHTTPClient(env('LINEBOT_CHANNEL_TOKEN'));
         $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINEBOT_CHANNEL_SECRET')]);
         $messageBuild = new TextMessageBuilder('Hello, test!');
-        return $lineBot->pushMessage(env('LINEBOT_CHANNEL_ID'), $messageBuild);
+        $lineBot->pushMessage(env('LINE_USER_ID'), $messageBuild);
+        return 'good';
     }
 
-    // public function webhook(Request $request)
-    // {
-    //     return $request->toArray;
-    // }
+    public function webhook(Request $request)
+    {
+        var_dump($request->toArray());
+        Storage::disk('public')->append('line.log', $request->toArray());
+        return response('good', 200)->withHeaders([
+            "Access-Control-Allow-Origin" => "*"
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('line/test', 'LineBotController@pushMessage');
-// Route::post('line/webhook', 'LineBotController@webhook');
+Route::post('line/webhook', 'LineBotController@webhook');
