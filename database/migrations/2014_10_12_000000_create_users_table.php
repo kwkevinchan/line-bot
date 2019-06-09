2e417eecd5b69af8a935e999bc4909a2a30cb04e
kwkevin <kwkevin.chan@gmail.com>
2019-06-09 21:11:53 +0800

    mysql varchar set length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->nullable();
-            $table->string('lineId')->unique();
+            $table->string('name', 255);
+            $table->string('email', 255)->nullable();
+            $table->string('lineId', 255)->unique();
             $table->timestamps();
         });
     }
